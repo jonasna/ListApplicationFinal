0ff377a81265efa90cf38c744955b61c3a463d4a
jonasna <jonasna93@gmail.com>
2018-07-11 13:59:53 +0200

    Added ffimageloading packages and added initial setup

--- ListApplicationFinal/ListApplicationFinal.Android/MainActivity.cs
+++ ListApplicationFinal/ListApplicationFinal.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using FFImageLoading.Forms.Platform;
 using Prism;
 using Prism.Ioc;
 
@@ -22,6 +23,8 @@ namespace ListApplicationFinal.Droid
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            CachedImageRenderer.Init(true);
 
             try
             {
---
+++ ListApplicationFinal/ListApplicationFinal/Markup/ImageResource.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using FFImageLoading.Svg.Forms;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ListApplicationFinal.Markup
+{
+    [ContentProperty("Source")]
+    public class ImageResource : IMarkupExtension
+    {
+        public string Source { get; set; }
+        private readonly string _base;
+
+        public ImageResource()
+        {
+            _base = Assembly.GetCallingAssembly().GetName().Name + ".Images.";
+        }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+                return null;
+
+            return SvgImageSource.FromResource(_base + Source);
+        }
+    }
+}

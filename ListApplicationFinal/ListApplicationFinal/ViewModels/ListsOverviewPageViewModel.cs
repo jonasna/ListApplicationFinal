4ebf7b64cdfc9d46fc24ff10f1be21902b7d4605
jonasna <jonasna93@gmail.com>
2018-07-13 02:59:43 +0200

    Progress. The xaml page is nearly complete

--- ListApplicationFinal/ListApplicationFinal/ViewModels/ListsOverviewPageViewModel.cs
+++ ListApplicationFinal/ListApplicationFinal/ViewModels/ListsOverviewPageViewModel.cs
@@ -1,4 +1,7 @@
-﻿using Prism.Navigation;
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+using ListApplicationFinal.Domain;
+using Prism.Navigation;
 
 namespace ListApplicationFinal.ViewModels
 {
@@ -6,6 +9,61 @@ namespace ListApplicationFinal.ViewModels
     {
         public ListsOverviewPageViewModel(INavigationService navigationService) : base(navigationService)
         {
+            if(Debugger.IsAttached)
+                TodoListsCollection = new ObservableCollection<TodoList>
+                {
+                    new TodoList("Indkøbdsliste", "Swagboy1337"),
+                    new TodoList("En til", "Swagboy1337"),
+                    new TodoList("Der var engang", "Swagboy1337"),
+                    new TodoList("Tralala", "Swagboy1337"),
+                    new TodoList("Bill me", "Swagboy1337"),
+                    new TodoList("Familiy Guy", "Swagboy1337"),
+                    new TodoList("Lol", "Swagboy1337"),
+                    new TodoList("Nope", "Swagboy1337"),
+                    new TodoList("My dinner", "Swagboy1337"),
+                    new TodoList("Nu er det vidst nok", "Swagboy1337"),
+                    new TodoList("Sidste", "Swagboy1337"),
+                    new TodoList("Indkøbdsliste", "Swagboy1337"),
+                    new TodoList("En til", "Swagboy1337"),
+                    new TodoList("Der var engang", "Swagboy1337"),
+                    new TodoList("Tralala", "Swagboy1337"),
+                    new TodoList("Bill me", "Swagboy1337"),
+                    new TodoList("Familiy Guy", "Swagboy1337"),
+                    new TodoList("Lol", "Swagboy1337"),
+                    new TodoList("Nope", "Swagboy1337"),
+                    new TodoList("My dinner", "Swagboy1337"),
+                    new TodoList("Nu er det vidst nok", "Swagboy1337"),
+                    new TodoList("Sidste", "Swagboy1337"),
+                    new TodoList("Indkøbdsliste", "Swagboy1337"),
+                    new TodoList("En til", "Swagboy1337"),
+                    new TodoList("Der var engang", "Swagboy1337"),
+                    new TodoList("Tralala", "Swagboy1337"),
+                    new TodoList("Bill me", "Swagboy1337"),
+                    new TodoList("Familiy Guy", "Swagboy1337"),
+                    new TodoList("Lol", "Swagboy1337"),
+                    new TodoList("Nope", "Swagboy1337"),
+                    new TodoList("My dinner", "Swagboy1337"),
+                    new TodoList("Nu er det vidst nok", "Swagboy1337"),
+                    new TodoList("Sidste", "Swagboy1337"),
+                    new TodoList("Indkøbdsliste", "Swagboy1337"),
+                    new TodoList("En til", "Swagboy1337"),
+                    new TodoList("Der var engang", "Swagboy1337"),
+                    new TodoList("Tralala", "Swagboy1337"),
+                    new TodoList("Bill me", "Swagboy1337"),
+                    new TodoList("Familiy Guy", "Swagboy1337"),
+                    new TodoList("Lol", "Swagboy1337"),
+                    new TodoList("Nope", "Swagboy1337"),
+                    new TodoList("My dinner", "Swagboy1337"),
+                    new TodoList("Nu er det vidst nok", "Swagboy1337"),
+                    new TodoList("Sidste", "Swagboy1337")
+                };
+        }
+
+        private ObservableCollection<TodoList> _todoListsCollection;
+        public ObservableCollection<TodoList> TodoListsCollection
+        {
+            get => _todoListsCollection;
+            set => SetProperty(ref _todoListsCollection, value);
         }
     }
 }
